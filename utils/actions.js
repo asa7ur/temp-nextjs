59b94869aee1f874bde14ce0ada30d82eb107501
Garik Asatryan <asa7ur@gmail.com>
2024-07-16 10:47:39 +0200

    added form editing functionality

--- utils/actions.js
+++ utils/actions.js
@@ -1,6 +1,7 @@
 'use server'
 import prisma from './db'
 import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
 
 export const getAllTasks = async () => {
   return await prisma.task.findMany({
@@ -21,9 +22,32 @@ export const createTask = async (formData) => {
 }
 
 export const deleteTask = async (formData) => {
-    const id = formData.get('id')
-    await prisma.task.delete({
-        where: {id}
-    })
-    revalidatePath('/tasks')
+  const id = formData.get('id')
+  await prisma.task.delete({
+    where: { id },
+  })
+  revalidatePath('/tasks')
+}
+
+export const getTask = async (id) => {
+  return prisma.task.findUnique({
+    where: { id },
+  })
+}
+
+export const editTask = async (formData) => {
+  const id = formData.get('id')
+  const content = formData.get('content')
+  const completed = formData.get('completed')
+
+  await prisma.task.update({
+    where: {
+      id,
+    },
+    data: {
+      content,
+      completed: completed === 'on' ? true : false,
+    },
+  })
+  redirect('/tasks')
 }
